94cc0201e3a4d528190d77c7b3c95f929bdacc46
ShwvaGanz <shawavatritya@outlook.co.id>
2026-01-08 14:19:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,7 +52,7 @@ run_silent() {
 
 clear
 echo -e "${BOLD}ZiVPN UDP Installer${RESET}"
-echo -e "${GRAY}AutoFTbot Edition${RESET}"
+echo -e "${GRAY}Shwtrya Edition${RESET}"
 echo ""
 
 if [[ "$(uname -s)" != "Linux" ]] || [[ "$(uname -m)" != "x86_64" ]]; then
